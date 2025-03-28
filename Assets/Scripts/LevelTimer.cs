519943216f7be44f950145f2e8b8c0966817009d
Ewoud Van Vooren <ewoud.vanvooren@gmail.com>
2025-03-28 17:30:38 -0400

    more level timer

--- Assets/Scripts/LevelTimer.cs
+++ Assets/Scripts/LevelTimer.cs
@@ -3,7 +3,8 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
 using System.IO;
-public class LevelTimer : MonoBehaviour {
+public class LevelTimer : MonoBehaviour
+{
     public TMP_Text currentLevelTimeText;
     public TMP_Text sessionTotalText;
     public Image goldDot;
@@ -17,29 +18,39 @@ public class LevelTimer : MonoBehaviour {
     private string levelTimesFile;
     private string sessionTotalFile;
     private float sessionTotalTime;
-    void Start() {
+    void Start()
+    {
         levelTimesFile = Application.persistentDataPath + "/LevelTimes.txt";
         sessionTotalFile = Application.persistentDataPath + "/SessionTotal.txt";
-        if (SceneManager.GetActiveScene().buildIndex == 0) {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
             if (File.Exists(levelTimesFile)) File.Delete(levelTimesFile);
             if (File.Exists(sessionTotalFile)) File.Delete(sessionTotalFile);
             sessionTotalTime = 0f;
             File.WriteAllText(sessionTotalFile, sessionTotalTime.ToString());
-        } else {
-            if (File.Exists(sessionTotalFile)) {
+        }
+        else
+        {
+            if (File.Exists(sessionTotalFile))
+            {
                 float.TryParse(File.ReadAllText(sessionTotalFile), out sessionTotalTime);
-            } else {
+            }
+            else
+            {
                 sessionTotalTime = 0f;
             }
         }
     }
-    void Update() {
-        if (!levelFinished) {
+    void Update()
+    {
+        if (!levelFinished)
+        {
             elapsedTime += Time.deltaTime;
             UpdateDisplays();
         }
     }
-    void UpdateDisplays() {
+    void UpdateDisplays()
+    {
         int m = (int)(elapsedTime / 60);
         int s = (int)(elapsedTime % 60);
         int ms = (int)((elapsedTime * 1000) % 1000);
@@ -51,35 +62,51 @@ public class LevelTimer : MonoBehaviour {
         int tms = (int)((total * 1000) % 1000);
         string formattedTotal = string.Format("{0:00}:{1:00}:{2:000}", tm, ts, tms);
         if (sessionTotalText != null) sessionTotalText.text = formattedTotal;
+        float silverThreshold = goldTimeThreshold * silverMultiplier;
+        float bronzeThreshold = goldTimeThreshold * bronzeMultiplier;
+        if (elapsedTime <= goldTimeThreshold) SetMedals(true, true, true);
+        else if (elapsedTime <= silverThreshold) SetMedals(false, true, true);
+        else if (elapsedTime <= bronzeThreshold) SetMedals(false, false, true);
+        else SetMedals(false, false, false);
     }
-    public void FinishLevel() {
+    public void FinishLevel()
+    {
         levelFinished = true;
         float silverThreshold = goldTimeThreshold * silverMultiplier;
         float bronzeThreshold = goldTimeThreshold * bronzeMultiplier;
         string medal = "";
-        if (elapsedTime <= goldTimeThreshold) {
+        if (elapsedTime <= goldTimeThreshold)
+        {
             medal = "Gold";
-            SetMedals(true,true,true);
-        } else if (elapsedTime <= silverThreshold) {
+            SetMedals(true, true, true);
+        }
+        else if (elapsedTime <= silverThreshold)
+        {
             medal = "Blue";
-            SetMedals(false,true,true);
-        } else if (elapsedTime <= bronzeThreshold) {
+            SetMedals(false, true, true);
+        }
+        else if (elapsedTime <= bronzeThreshold)
+        {
             medal = "Red";
-            SetMedals(false,false,true);
-        } else {
+            SetMedals(false, false, true);
+        }
+        else
+        {
             medal = "None";
-            SetMedals(false,false,false);
+            SetMedals(false, false, false);
         }
         sessionTotalTime += elapsedTime;
         File.WriteAllText(sessionTotalFile, sessionTotalTime.ToString());
-        SaveLevelData(medal);
+        SaveLevelTime(medal);
     }
-    void SetMedals(bool showGold, bool showBlue, bool showRed) {
+    void SetMedals(bool showGold, bool showBlue, bool showRed)
+    {
         if (goldDot != null) goldDot.enabled = showGold;
         if (blueDot != null) blueDot.enabled = showBlue;
         if (redDot != null) redDot.enabled = showRed;
     }
-    void SaveLevelData(string medal) {
+    void SaveLevelTime(string medal)
+    {
         string levelName = SceneManager.GetActiveScene().name;
         int m = (int)(elapsedTime / 60);
         int s = (int)(elapsedTime % 60);
